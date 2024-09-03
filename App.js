e832334c28992f6e47c8f20c2eb1bfd3d7cffb24
Nixxa <52290162+sogeking-pixel@users.noreply.github.com>
2024-09-02 22:51:43 -0500

    Api

--- App.js
+++ App.js
@@ -1,39 +1,79 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, FlatList, Modal, Button, TouchableOpacity } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';// Importar iconos de Ionicons
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  FlatList,
+  Modal,
+  Button,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
-import logoAgen from './assets/logoAgen.png'; 
-import imagen1 from './assets/imagen1.jpg'; 
-import imagen2 from './assets/imagen2.jpg';
-import imagen3 from './assets/imagen3.jpg';
-import InformationScreen from './InformationScreen';
-import MapScreen from './MapScreen'; 
+import logoAgen from "./assets/logoAgen.png";
+import imagen1 from "./assets/imagen1.jpg";
+import imagen2 from "./assets/imagen2.jpg";
+import imagen3 from "./assets/imagen3.jpg";
+import InformationScreen from "./InformationScreen";
+import MapScreen from "./MapScreen";
 
 const Stack = createNativeStackNavigator();
 
 const HomeScreen = ({ navigation }) => {
   // Datos de las imágenes
   const imageData = [
-    { id: '1', source: imagen1, label: 'Chiclayo' },
-    { id: '2', source: imagen2, label: 'Lima' },
-    { id: '3', source: imagen3, label: 'Lima' },
-    { id: '4', source: imagen1, label: 'Chiclayo' },
-    { id: '5', source: imagen2, label: 'Lima' },
-    { id: '6', source: imagen3, label: 'Lima' },
-    { id: '7', source: imagen1, label: 'Chiclayo' },
-    { id: '8', source: imagen2, label: 'Lima' },
-    { id: '9', source: imagen3, label: 'Lima' },
+    { id: "chiclayo", source: imagen1, label: "Chiclayo" },
+    { id: "lima", source: imagen2, label: "Lima" },
+    { id: "lima", source: imagen3, label: "Lima" },
+    { id: "lima", source: imagen1, label: "Chiclayo" },
+    { id: "lima", source: imagen2, label: "Lima" },
+    { id: "lima", source: imagen3, label: "Lima" },
+    { id: "lima", source: imagen1, label: "Chiclayo" },
+    { id: "lima", source: imagen2, label: "Lima" },
+    { id: "lima", source: imagen3, label: "Lima" },
   ];
 
-  // Estado para controlar la visibilidad del modal
+  // Estados para el modal y los datos de la API
   const [modalVisible, setModalVisible] = React.useState(false);
+  const [modalData, setModalData] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+
+  // Función para manejar la apertura del modal y la carga de datos
+  const handlePress = async (item) => {
+    setLoading(true);
+    setModalVisible(true);
+
+    try {
+      // Aquí se haría la llamada a la API, asumiendo que 'id' es el parámetro necesario.
+      const baseUrl = "https://www.meteosource.com/api/v1/free/point";
+      const params = {
+        place_id: item.id,
+        sections: "all",
+        timezone: "UTC",
+        language: "en",
+        units: "metric",
+        key: "rtapw9gj1a6mhbhu5x8x7owpl4ybhzfppecuwjvj",
+      };
+      const queryString = new URLSearchParams(params).toString();
+      const url = `${baseUrl}?${queryString}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setModalData(data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   // Función para renderizar cada elemento de la lista
   const renderItem = ({ item }) => (
-    <TouchableOpacity onPress={() => setModalVisible(true)}>
+    <TouchableOpacity onPress={() => handlePress(item)}>
       <View style={styles.imageContainer}>
         <Image source={item.source} style={styles.image} />
         <Text style={styles.textOverlay}>{item.label}</Text>
@@ -49,19 +89,33 @@ const HomeScreen = ({ navigation }) => {
 
       {/* Contenedor de Título y Íconos */}
       <View style={styles.headerContainer}>
-        {/* Contenedor de Título y Fecha */}
         <View style={styles.titleContainer}>
           <Text style={styles.titleText}>Chiclayo a Lima</Text>
           <Text style={styles.dateText}>Agosto 31 - Octubre 11</Text>
         </View>
-
-        
         <View style={styles.iconContainer}>
-          <TouchableOpacity onPress={() => navigation.navigate('InformationScreen')}>
-            <Ionicons name="information-circle-outline" size={24} color="black" style={styles.icon} />
+          <TouchableOpacity
+            onPress={() => navigation.navigate("InformationScreen")}
+          >
+            <Ionicons
+              name="information-circle-outline"
+              size={24}
+              color="black"
+              style={styles.icon}
+            />
           </TouchableOpacity>
-          <Ionicons name="document-text-outline" size={24} color="black" style={styles.icon} />
-          <Ionicons name="settings-outline" size={24} color="black" style={styles.icon} />
+          <Ionicons
+            name="document-text-outline"
+            size={24}
+            color="black"
+            style={styles.icon}
+          />
+          <Ionicons
+            name="settings-outline"
+            size={24}
+            color="black"
+            style={styles.icon}
+          />
         </View>
       </View>
 
@@ -69,7 +123,7 @@ const HomeScreen = ({ navigation }) => {
       <FlatList
         data={imageData}
         renderItem={renderItem}
-        keyExtractor={item => item.id}
+        keyExtractor={(item) => item.id}
         contentContainerStyle={styles.listContent}
       />
 
@@ -84,10 +138,33 @@ const HomeScreen = ({ navigation }) => {
       >
         <View style={styles.modalOverlay}>
           <View style={styles.modalContent}>
-            <Text style={styles.modalText}>
-              "No se pudo completar la operación. Por favor, revisa los detalles y vuelve a intentarlo."
-            </Text>
-            <Button title="Cerrar" onPress={() => setModalVisible(false)} />
+            {loading ? (
+              <ActivityIndicator size="large" color="#0000ff" />
+            ) : (
+              <>
+                {modalData ? (
+                  <>
+                    <Text style={styles.modalText}>
+                      Clima: {modalData.hourly.data[0].weather}
+                    </Text>
+                    <Text style={styles.modalText}>
+                      Fecha Actual: {modalData.daily.data[0].day}
+                    </Text>
+                    <Text style={styles.modalText}>
+                      Temperatura: {modalData.current.temperature}
+                    </Text>
+                    <Text style={styles.modalText}>
+                      Viento: {modalData.current.wind.speed}
+                    </Text>
+                  </>
+                ) : (
+                  <Text style={styles.modalText}>
+                    No se pudo cargar la información.
+                  </Text>
+                )}
+                <Button title="Cerrar" onPress={() => setModalVisible(false)} />
+              </>
+            )}
           </View>
         </View>
       </Modal>
@@ -99,9 +176,16 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen  name="Home" component={HomeScreen} />
-        <Stack.Screen name="InformationScreen" component={InformationScreen} options={{ title: 'Información' }} />
-        <Stack.Screen name="MapScreen" component={MapScreen} options={{ title: 'Map' }}
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="InformationScreen"
+          component={InformationScreen}
+          options={{ title: "Información" }}
+        />
+        <Stack.Screen
+          name="MapScreen"
+          component={MapScreen}
+          options={{ title: "Map" }}
         />
       </Stack.Navigator>
     </NavigationContainer>
@@ -113,76 +197,74 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: 40, // Espacio superior para el logo
-    
+    backgroundColor: "#fff",
+    paddingTop: 40,
   },
-  
   logo: {
     width: 200,
     height: 200,
-    resizeMode: 'contain',
-    alignSelf: 'center',
-    marginBottom: 10, // Espacio entre el logo y el resto del contenido
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginBottom: 10,
   },
   headerContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     paddingHorizontal: 10,
-    marginBottom: 10, // Espacio entre el header y las imágenes
+    marginBottom: 10,
   },
   titleContainer: {
-    flexDirection: 'column',
+    flexDirection: "column",
   },
   titleText: {
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   dateText: {
     fontSize: 14,
-    color: 'gray',
+    color: "gray",
   },
   iconContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   icon: {
-    marginHorizontal: 5, // Espacio entre los iconos
+    marginHorizontal: 5,
   },
   listContent: {
-    paddingHorizontal: 10, // Espacio horizontal para la lista
+    paddingHorizontal: 10,
   },
   imageContainer: {
     marginBottom: 10,
-    position: 'relative',
+    position: "relative",
   },
   image: {
-    width: '100%',
+    width: "100%",
     height: 150,
-    borderRadius: 10, // Bordes redondeados para las imágenes
+    borderRadius: 10,
   },
   textOverlay: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 10,
     left: 10,
-    color: 'white',
-    fontWeight: 'bold',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Fondo semi-transparente para el texto
+    color: "white",
+    fontWeight: "bold",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
     paddingHorizontal: 5,
     borderRadius: 5,
   },
   modalOverlay: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Fondo semi-transparente
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
   },
   modalContent: {
     width: 300,
     padding: 20,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 10,
-    alignItems: 'center',
+    alignItems: "center",
   },
   modalText: {
     fontSize: 18,
